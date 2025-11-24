11eb9d13c7638ab7ac8e3614d4f5b99d97569f93
kanann05 <148896349+kanann05@users.noreply.github.com>
2025-11-24 16:34:31 +0530

    first

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import global_en from "./locales/en/en.json";
+import global_mar from "./locales/mar/mar.json";
+import global_hin from "./locales/hin/hin.json";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+// import { LanguageProvider } from "./context/LanguageContext.js";
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    mar: {
+      global: global_mar,
+    },
+    hin: {
+      global: global_hin,
+    },
+  },
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <LanguageProvider> */}
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+    {/* </LanguageProvider> */}
+  </StrictMode>
+);
